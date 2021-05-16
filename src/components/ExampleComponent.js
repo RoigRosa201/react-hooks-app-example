6b2e3f3d3db75be1a149d895c14d27b8c5f8bf9b
roig <roigrosa88@gmail.com>
2021-05-16 01:48:42 -0400

    tests done

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -5,11 +5,13 @@ function ExampleComponent() {
   return (
     <div className="example">
       <img
+      
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
     </div>
+    
   );
 }
 
